57b4c8435ebcf4d4fd09a774b78f0e8607d7f3f0
Thomas <thomtrepanier@gmail.com>
2020-09-28 14:08:05 -0400

    Line ending fixed

--- exercice.py
+++ exercice.py
@@ -8,22 +8,23 @@ import itertools
 
 
 def get_maximums(numbers):
-	return [max(list) for list in numbers]
+    return [max(list) for list in numbers]
 
 
 def join_integers(numbers):
-	return int("".join([str(number) for number in numbers]))
+    return int("".join([str(number) for number in numbers]))
 
 
 def generate_prime_numbers(limit):
-	prime = []
-	numbers = range(2, limit)
-	while len(numbers) > 0:
-		nextPrime = numbers[0]
-		prime.append(nextPrime)
+    prime = []
+    numbers = range(2, limit)
+    while len(numbers) > 0:
+        nextPrime = numbers[0]
+        prime.append(nextPrime)
 
-		numbers = [i for i in range(2, numbers[0] + 1) if numbers[0] % i != 0]
-	return prime
+        numbers = [i for i in range(2, numbers[0] + 1) if numbers[0] % i != 0]
+    return prime
+
 
 def combine_strings_and_numbers(strings, num_combinations, excluded_multiples):
     # Une ligne (+ Cool)
@@ -31,6 +32,7 @@ def combine_strings_and_numbers(strings, num_combinations, excluded_multiples):
             for i in range(1, num_combinations + 1)
             if excluded_multiples is None or i % excluded_multiples != 0
             for letter in strings]
+
 
 if __name__ == "__main__":
     print(get_maximums([[1, 2, 3], [6, 5, 4], [10, 11, 12], [8, 9, 7]]))
