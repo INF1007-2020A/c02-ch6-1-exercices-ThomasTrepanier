71c68ecb3a0a4243f3b5abcf9f2902246529bca1
Thomas <thomtrepanier@gmail.com>
2020-09-28 14:06:52 -0400

    Exercices finis!

--- exercice.py
+++ exercice.py
@@ -8,24 +8,38 @@ import itertools
 
 
 def get_maximums(numbers):
-	return [0]
+    return [max(list) for list in numbers]
+
 
 def join_integers(numbers):
-	return 0
+    return int("".join([str(number) for number in numbers]))
+
 
 def generate_prime_numbers(limit):
-	return [0]
+    prime = []
+    numbers = list(range(2, limit + 1))
+    while len(numbers) > 0:
+        nextPrime = numbers[0]
+        prime.append(nextPrime)
+
+        numbers = [i for i in numbers if i % nextPrime != 0]
+    return prime
+
 
 def combine_strings_and_numbers(strings, num_combinations, excluded_multiples):
-	return [""]
+    # Une ligne (+ Cool)
+    return [letter + str(i)
+            for i in range(1, num_combinations + 1)
+            if excluded_multiples is None or i % excluded_multiples != 0
+            for letter in strings]
 
 if __name__ == "__main__":
-	print(get_maximums([[1,2,3], [6,5,4], [10,11,12], [8,9,7]]))
-	print("")
-	print(join_integers([111, 222, 333]))
-	print(join_integers([69, 420]))
-	print("")
-	print(generate_prime_numbers(17))
-	print("")
-	print(combine_strings_and_numbers(["A", "B"], 2, None))
-	print(combine_strings_and_numbers(["A", "B"], 5, 2))
+    print(get_maximums([[1, 2, 3], [6, 5, 4], [10, 11, 12], [8, 9, 7]]))
+    print("")
+    print(join_integers([111, 222, 333]))
+    print(join_integers([69, 420]))
+    print("")
+    print(generate_prime_numbers(17))
+    print("")
+    print(combine_strings_and_numbers(["A", "B"], 2, None))
+    print(combine_strings_and_numbers(["A", "B"], 5, 2))
